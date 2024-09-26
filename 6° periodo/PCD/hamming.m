6a633312a69be379ada9ec63480e826d75d43f1d
iamjuli0 <135247244+iamjuli0@users.noreply.github.com>
2024-09-26 16:58:14 -0400

    Update hamming.m

--- 6° periodo/PCD/hamming.m
+++ 6° periodo/PCD/hamming.m
@@ -1,11 +1,11 @@
-% -----------------------------
+% -----------------------
 % ATUALIZACAO 26/09/2024
-% -----------------------------
+% -----------------------
 
-% Funcao Principal
-
-function saida = hamming(k,n)
- indice_paridade = bits_paridade(k) - 1;
+% >>>>>>>>>>>>>>>>>>>>>>>>>>> Funcoes Principais <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+% ---------------------------- Geradora de Hamming ---------------------------------
+function saida = hammingGeradora(k,n)
+ indice_paridade = bitsParidade(k) - 1;
  identidade = eye(n);
  matriz_paridade = [];
 
@@ -26,22 +26,50 @@ function saida = hamming(k,n)
  
  saida = [identidade, transpose(matriz_paridade)];
 end
+% ---------------------------- Verificação de Paridade Hamming ---------------------
+function saida = hammingVerificacao(matriz)
+     [linhas, colunas] = size(matriz);
+     matriz_paridade = matriz(1:linhas, (linhas+1):colunas);
+     transposta_paridade = transpose(matriz_paridade);
+     identidade = eye(colunas-linhas);
+     saida = [transposta_paridade, identidade];
+end
+% ------------------------- Distância mínima do codificador ------------------------
+function saida = distMinima(matriz)
+     vetor = [];
+     [linhas, colunas] = size(matriz);
 
-% Funcoes Secundarias
+     for i = 1:linhas
+         linha_atual = matriz(i, :);
+         peso = sum(linha_atual(:));
+         vetor = [vetor, [peso]];
+     end
 
-function resultado = EhPotencia(k,pot)
-
- if k > 0 && pot > 1
-     log_result = log(k) / log(pot);  % calcula o logaritmo na base desejada
-     resultado = (floor(log_result) == log_result);  % verifica se é um inteiro
-     else
-         resultado = false;  % é inválido
-    end
-
+     saida = min(vetor(:));
+end
+% ------------------------- Detecção e Correção de Erros ---------------------------
+function saida = detecErros(matriz)
+     dist = distMinima(matriz);
+     saida = floor(dist-1);
 end
 
-function quantidade = bits_paridade(k)
-
+function saida = correcErros(matriz)
+     dist = distMinima(matriz);
+     saida = floor((dist-1)/2);
+end
+% >>>>>>>>>>>>>>>>>>>>>>>>>>> Funcoes Secundarias <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+% ----------------- Verifica se um número é potencia de outro ----------------------
+function resultado = EhPotencia(k,pot)
+if k > 0 && pot > 1
+     log_result = log(k) / log(pot);  % calcula o logaritmo na base desejada
+     resultado = (floor(log_result) == log_result);  % verifica se é um inteiro
+     
+     else
+         resultado = false;  % é inválido
+     end
+end
+% ------ Verifica a quantidade de bits de paridade de um código com saída k --------
+function quantidade = bitsParidade(k)
      quantidade = 0;
      for i = 1:k
          if EhPotencia(i,2)
@@ -49,9 +77,9 @@ function quantidade = bits_paridade(k)
          end
      end
 end
-
-function trans = transformada(valor, paridade)
-     trans = dec2bin(valor);
+% ---- Verifica se tal valor k tem o bits significativos em função da paridade -----
+function trans = transformada(k, paridade)
+     trans = dec2bin(k);
      trans = str2num(trans);
      trans = trans/(10^paridade);
      trans = floor(trans);
