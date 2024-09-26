15b10fe4231a455de0baaaa350d73a5bcf36c2e6
iamjuli0 <135247244+iamjuli0@users.noreply.github.com>
2024-09-26 15:41:10 -0400

    Update hamming.m

--- 6° periodo/PCD/hamming.m
+++ 6° periodo/PCD/hamming.m
@@ -1,34 +1,3 @@
-% Funcao Principal
-
-function saida = hamming(k,n)
- paridade_indice = k - n;
- identidade = eye(n);
- saida = identidade;
- 
- for i = 1:k
-     teste = i;
-     if (teste == 1 || EhPotencia(teste,2))
-         continue;
-     end
-     
-     trans = dec2bin(i);
-     display(trans);
- end
- 
-end
-
-% Funcoes Secundarias
-
-function resultado = EhPotencia(k,pot)
-
- if k > 0 && pot > 1
-     log_result = log(k) / log(pot);  % calcula o logaritmo na base desejada
-     resultado = (floor(log_result) == log_result);  % verifica se é um inteiro
-     else
-         resultado = false;  % é inválido
-    end
-
-end
 % -----------------------------
 % ATUALIZACAO 26/09/2024
 % -----------------------------
@@ -36,10 +5,9 @@ end
 % Funcao Principal
 
 function saida = hamming(k,n)
- paridade_indice = k - n;
+ indice_paridade = bits_paridade(k) - 1;
  identidade = eye(n);
  matriz_paridade = [];
- saida = identidade;
 
  for i = 1:k
      teste = i;
@@ -47,16 +15,16 @@ function saida = hamming(k,n)
          continue;
      end
 
-     trans = dec2bin(i);
-     trans = str2num(trans);
-     trans = trans/1;
-     trans = floor(trans);
-     trans = mod(trans,10);
-     matriz_paridade = [matriz_paridade, [trans]];
+     coluna_atual = [];
+     for j = 0:indice_paridade
+         elemento = transformada(i, j);
+         coluna_atual = [coluna_atual; elemento];
+     end
+     
+     matriz_paridade = [matriz_paridade, coluna_atual];
  end
  
- display(matriz_paridade);
-
+ saida = transpose(matriz_paridade);
 end
 
 % Funcoes Secundarias
@@ -70,4 +38,22 @@ function resultado = EhPotencia(k,pot)
          resultado = false;  % é inválido
     end
 
+end
+
+function quantidade = bits_paridade(k)
+
+     quantidade = 0;
+     for i = 1:k
+         if EhPotencia(i,2)
+             quantidade++;
+         end
+     end
+end
+
+function trans = transformada(valor, paridade)
+     trans = dec2bin(valor);
+     trans = str2num(trans);
+     trans = trans/(10^paridade);
+     trans = floor(trans);
+     trans = mod(trans,10);
 end
