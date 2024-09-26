b3e69de4db56c5aa48594317def6774388e7b9ed
iamjuli0 <135247244+iamjuli0@users.noreply.github.com>
2024-09-26 11:53:50 -0400

    Update hamming.m

--- 6° periodo/PCD/hamming.m
+++ 6° periodo/PCD/hamming.m
@@ -29,3 +29,44 @@ function resultado = EhPotencia(k,pot)
     end
 
 end
+
+% Funcao Principal
+
+function saida = hamming(k,n)
+ paridade_indice = k - n;
+ identidade = eye(n);
+ matriz_paridade = [];
+ saida = identidade;
+
+ for i = 1:k
+     teste = i;
+     if (teste == 1 || EhPotencia(teste,2))
+         continue;
+     end
+
+     trans = dec2bin(i);
+     trans = str2num(trans);
+     trans = trans/1;
+     trans = floor(trans);
+     trans = mod(trans,10);
+     matriz_paridade = [matriz_paridade, [trans]];
+ end
+ 
+ display(matriz_paridade);
+
+end
+
+% ATUALIZACAO 26/09/2024
+
+% Funcoes Secundarias
+
+function resultado = EhPotencia(k,pot)
+
+ if k > 0 && pot > 1
+     log_result = log(k) / log(pot);  % calcula o logaritmo na base desejada
+     resultado = (floor(log_result) == log_result);  % verifica se é um inteiro
+     else
+         resultado = false;  % é inválido
+    end
+
+end
