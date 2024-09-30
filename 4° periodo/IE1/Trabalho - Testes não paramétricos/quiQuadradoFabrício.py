acf3c9f0b144421b4023b8e3f8cd978f0d59d5a9
iamjuli0 <135247244+iamjuli0@users.noreply.github.com>
2024-09-30 11:24:43 -0400

    Update quiQuadradoFabrício.py

--- 4° periodo/IE1/Trabalho - Testes não paramétricos/quiQuadradoFabrício.py
+++ 4° periodo/IE1/Trabalho - Testes não paramétricos/quiQuadradoFabrício.py
@@ -2,3 +2,13 @@ from scipy.stats import mannwhitneyu
 
 homens = [4, 4, 5, 5, 3, 3, 2, 1, 4, 5, 2]
 mulheres = [4, 5, 5, 5, 4, 3, 3, 5, 2, 1, 4]
+
+estatistica, p_valor = mannwhitneyu(mulheres, homens)
+
+print("Estatística do teste de Mann-Whitney:", estatistica)
+print("Significância:", p_valor)
+
+if p_valor < 0.05:
+  print("Há uma diferença significativa entre as avaliações entre homens e mulheres.")
+else:
+  print("Tanto homens quanto mulheres possuem avaliações semelhantes")
